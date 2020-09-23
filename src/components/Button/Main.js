c01315d9c0df7c1cdf8c860cc65ce4d43b4bbf58
Oleh Dzenhliuk <dzenglyuk23@ukr.net>
2020-09-23 12:26:39 +0300

    NavBar created

---
+++ src/components/Button/Main.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "./Main.css";
+
+const STYLES = ["btn--primary", "btn--outline"];
+const SIZES = ["btn--medium", "btn--large", "btn--modile"];
+const COLORS = ["primary", "blue", "red", "green"];
+
+const Button = ({ children, type, onClick, btnStyle, btnSize, btnColor }) => {
+    const checkProp = (propName, propValue, name) => {
+		let defaultProp = propName[0];
+		if (name && name === 'color') defaultProp = '';
+		return propName.includes(propValue) ? propValue : defaultProp;
+    };
+    return (
+        <button
+            className={`btn 
+			${checkProp(STYLES, btnStyle)} 
+			${checkProp(SIZES, btnSize)} 
+			${checkProp(COLORS, btnColor, 'color')}`}
+            onClick={onClick}
+            type={type}
+        >
+            {children}
+        </button>
+    );
+};
+
+Button.defaultProps = {
+    btnStyle: "btn--primary",
+    btnSize: null,
+    btnColor: null
+}
+
+export default React.memo(Button);
---
+++ src/core/Router/Main.js
@@ -0,0 +1,24 @@
+import React, { useMemo } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from '../../components/Navbar/Main';
+
+import pages from '../paths/index';
+
+const Main = ({ Navbar, pages }) => {
+    const thisPages = useMemo(() => pages || [], [ pages ]);
+    return (
+        <Router>
+            <Navbar pages={thisPages}/>
+            <Switch>
+                {thisPages.map((page, index) => <Route key={index} exact {...page} />)}
+            </Switch>
+        </Router>
+    );
+}
+
+Main.defaultProps = {
+    Navbar,
+    pages
+}
+
+export default React.memo(Main);
