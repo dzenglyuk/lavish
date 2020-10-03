b6898caaf694d3977e3c7814d9310faa142129e5
Oleh Dzenhliuk <dzenglyuk23@ukr.net>
2020-10-03 13:41:56 +0300

    Revert "Feature/pages"

--- src/core/paths/index.js
+++
@@ -1,9 +0,0 @@
-import HomePage from "../../pages/HomePage/Main";
-
-const pages = [
-    { path: '/', title: 'Home', component: HomePage },
-    { path: '/services', title: 'Services' },
-    { path: '/products', title: 'Products' }
-];
-
-export default pages;
