647cff9c23acd7da0f6dcbe98588e91083eb8af5
Oleh Dzenhliuk <dzenglyuk23@ukr.net>
2020-10-04 14:55:55 +0300

    Services and Products pages added

--- src/core/paths/index.js
+++ src/core/paths/index.js
@@ -1,9 +1,11 @@
 import HomePage from "../../pages/HomePage/Main";
+import ServicesPage from "../../pages/ServicesPage/Main";
+import ProductsPage from "../../pages/ProductsPage/Main";
 
 const pages = [
     { path: '/', title: 'Home', component: HomePage },
-    { path: '/services', title: 'Services' },
-    { path: '/products', title: 'Products' }
+    { path: '/services', title: 'Services', component: ServicesPage },
+    { path: '/products', title: 'Products', component: ProductsPage }
 ];
 
 export default pages;
